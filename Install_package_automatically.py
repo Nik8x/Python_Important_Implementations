73f3c846408b10618dff645ac6d103360188d3be
Niket Choudhary <30778529+Nik8x@users.noreply.github.com>
2019-05-09 12:29:06 -0400

    Update Install_package_automatically.py

--- Install_package_automatically.py
+++ Install_package_automatically.py
@@ -4,3 +4,18 @@ if any(str(x == 'xgboost')):
     pass
 else:
     !pip install xgboost
+
+## or
+
+# to automatically check package & install if not present
+import sys
+import types
+import pip
+
+if pip.__version__ >= "10.0.0":
+    from pip._internal.utils.misc import get_installed_distributions
+else:
+    from pip import get_installed_distributions
+    
+if not('xgboost' in [package.project_name for package in get_installed_distributions()]):
+    !pip install xgboost -q
