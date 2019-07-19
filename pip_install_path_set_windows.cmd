803d52a77febdb53c0a74b63700e82975ae1b814
Niket Choudhary <30778529+Nik8x@users.noreply.github.com>
2019-07-19 12:25:36 -0400

    Update pip_install_path_set_windows.cmd

--- pip_install_path_set_windows.cmd
+++ pip_install_path_set_windows.cmd
@@ -1,5 +1,5 @@
-// install pip in windows for python3 with this command
+:: install pip in windows for python3 with this command
 python -m pip install --user --upgrade pip
 
-// if after installing pip it gives unknown command error, do
+:: if after installing pip it gives unknown command error, do
 set PATH=%PATH%;C:\Users\<username>\AppData\Local\Programs\Python\Python37-32\Scripts
