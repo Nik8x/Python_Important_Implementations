e7790438481a38d5b06f47c8e0042679568006eb
Niket Choudhary <30778529+Nik8x@users.noreply.github.com>
2019-08-01 21:46:40 -0400

    Create Read_Dump_JSON_Python.py

---
+++ Read_Dump_JSON_Python.py
@@ -0,0 +1,11 @@
+# To save data in JSON format 
+import json
+with open('my_dict.json', 'w') as f:
+    json.dump(my_dict, f)
+    
+# To read data from a JSON file
+import json
+f = open('my_dict.json')
+my_dict = json.load(f)
+# when finished, close the file
+f.close()
