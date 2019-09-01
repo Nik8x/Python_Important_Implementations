e44f6e3b255ff1e931689d389ea573cb4f470cf5
Niket Choudhary <30778529+Nik8x@users.noreply.github.com>
2019-09-01 19:44:54 -0400

    Create fake_data_using_faker.py

---
+++ fake_data_using_faker.py
@@ -0,0 +1,9 @@
+# Faker’s documentation https://faker.readthedocs.io/en/master/
+# Faker is a Python package that generates fake data for you.
+
+# !pip install Faker
+from faker import Faker
+fake = Faker()
+
+fake.name()
+fake.address()
